23e5b6c196edb1289ae9f3d16ced005b67e7a48d
Gabe R. <9311953+marsfan@users.noreply.github.com>
2025-12-17 22:39:09 -0700

    Inline the half adders into the full adder
    Signed-off-by: Gabe R. <9311953+marsfan@users.noreply.github.com>

--- vhdl/full_adder.vhd
+++ vhdl/full_adder.vhd
@@ -18,40 +18,12 @@ end FULL_ADDER;
 
 architecture RTL of FULL_ADDER is
 
-    -- Intermediate carry out signals
-    signal SUM_INTERMEDIATE : std_logic;
-    signal C_OUT_1          : std_logic;
-    signal C_OUT_2          : std_logic;
-
-    component HALF_ADDER is
-        port (
-            A     : in std_logic;
-            B     : in std_logic;
-            SUM   : out std_logic;
-            CARRY : out std_logic
-        );
-    end component HALF_ADDER;
-
 begin
 
-    -- First of the half-adders
-    first_half : HALF_ADDER port map (
-        A => A,
-        B => B,
-        SUM => SUM_INTERMEDIATE,
-        CARRY => C_OUT_1
-    );
+    SUM <= A XOR B XOR C_IN;
 
-    -- Second of the half-adders
-    second_half : HALF_ADDER port map (
-        A => SUM_INTERMEDIATE,
-        B => C_IN,
-        SUM => SUM,
-        CARRY => C_OUT_2
-    );
+    CARRY <= (A AND B) or (A AND C_IN) or (B AND C_IN);
 
-    -- Final carry is if either half adder carry is true
-    CARRY <= C_OUT_1 OR C_OUT_2;
 
 end architecture RTL;
 
