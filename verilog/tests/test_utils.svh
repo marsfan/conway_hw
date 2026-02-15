31238578c8cef2e4d80058abe1df33e68e0d3cb3
Gabe R. <9311953+marsfan@users.noreply.github.com>
2026-02-14 23:28:42 -0700

    Add comment to help suppress linting
    Signed-off-by: Gabe R. <9311953+marsfan@users.noreply.github.com>

--- verilog/tests/test_utils.svh
+++ verilog/tests/test_utils.svh
@@ -9,7 +9,7 @@
 `define CHECK_EQ(A, B, MSG) assert (A == B) else $fatal(2, MSG)
 
 /// Drive clock high for T time units, then drive low for T Time units
-`define RUN_CLOCK(CLK, T) begin #T CLK <= 1; #T CLK <= 0; end
+`define RUN_CLOCK(CLK, T) begin /*Run Clock Once*/ #T CLK <= 1; #T CLK <= 0; end
 
 
 `endif
